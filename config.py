c6cf0685033f692f55f3817cc8109fa7071717c2
umaru <104628168+umaru-233@users.noreply.github.com>
2025-02-03 23:03:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # DeepSeek API 配置
 DEEPSEEK_API_URL = "https://api.siliconflow.cn/v1/chat/completions"
-DEEPSEEK_API_KEY = 'sk-pnqdtbrfrnfcytmwszyeybmavyjzujgrkyqmkaekhijzsxjq'
+DEEPSEEK_API_KEY = 'sk-'
 #单次回复最大token
 MAX_TOKEN = 2000
 #温度
