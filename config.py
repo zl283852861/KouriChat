aad2efb6dfaaf59c210d745262aa934536d1e30f
umaru <104628168+umaru-233@users.noreply.github.com>
2025-02-03 23:02:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# DeepSeek API 配置
+DEEPSEEK_API_URL = "https://api.siliconflow.cn/v1/chat/completions"
+DEEPSEEK_API_KEY = 'sk-pnqdtbrfrnfcytmwszyeybmavyjzujgrkyqmkaekhijzsxjq'
+#单次回复最大token
+MAX_TOKEN = 2000
+#温度
+TEMPERATURE = 1.2
